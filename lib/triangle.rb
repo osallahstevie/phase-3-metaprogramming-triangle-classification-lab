cd4a385923ba27649955dcf3c804888d0a8103e4
osallahstevie <osallahstevie@gmail.com>
2023-02-15 15:04:48 +0300

    completed triangle classification lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
